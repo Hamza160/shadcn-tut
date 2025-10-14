d0305af4b352e48546a8c252b2e200c4d5145426
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-14 17:30:12 +0500

    Layout and Navbar Continue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import CustomButton from "@/components/CustomButton";
 
-export default function HomePage() {
-    return (
-        <div className="flex items-center justify-center w-full h-screen">
-            <CustomButton isRounded={true} disabled={false}/>
-        </div>
-    );
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  );
 }
 
+export default HomePage;
