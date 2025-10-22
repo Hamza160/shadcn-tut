b836d09ebd1497e0e5bad60b0c27ccf1e52d3a0f
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-23 01:50:34 +0500

    Todo List Continue

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {Checkbox} from "@/components/ui/checkbox";
+import {Label} from "@/components/ui/label";
 
 const TodoList = () => {
   return (
     <div className="">
         Calendar
         {/* LIST */}
-
+        <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
+            <div className="flex items-center gap-3">
+                <Checkbox id="terms" />
+                <Label htmlFor="terms">Accept terms and conditions</Label>
+            </div>
+        </ScrollArea>
     </div>
   );
 }
